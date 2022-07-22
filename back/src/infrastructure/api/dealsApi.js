14a4f0391a22e3b715138854ba5fc9e308fb1285
jungTH13 <infinite1305@gmail.com>
2022-07-22 09:50:25 +0900

    fix:api 모듈의 비정상적 에러처리 로직 수정

--- back/src/infrastructure/api/dealsApi.js
+++ back/src/infrastructure/api/dealsApi.js
@@ -8,10 +8,10 @@ dotenv.config();
 const type1 = async (api_url, params) => {
     try {
         const res = await axios.get(api_url, { params })
-            .then((res) => res)
-            .catch(() => RESPONSE.API_TIMEOUT);
+        // .then((res) => res)
+        // .catch(() => RESPONSE.API_TIMEOUT);
         if (res.status !== 200) {
-            return RESPONSE.API_RESPONSE_ERROR;
+            throw new Error(`올바른 데이터가 수신되지 않았습니다.[status:${result.status}]`);
         }
         if (res.data.response.header.resultCode === '00') {
             const result = RESPONSE.SUCCESS;
@@ -23,9 +23,9 @@ const type1 = async (api_url, params) => {
             return RESPONSE.API_ACCESS_DENIAL;
         }
 
-        return RESPONSE.API_RESPONSE_ERROR;
+        throw new Error(`올바른 데이터가 수신되지 않았습니다.[resultCode:${res.data.response.header.resultCode}]`);
     } catch (error) {
-        RESPONSE.errorCheckAndloggingThenThrow(error, RESPONSE.API_ERROR);
+        RESPONSE.errorCheckAndloggingThenThrow(error, RESPONSE.API_RESPONSE_ERROR);
     }
 };
 
